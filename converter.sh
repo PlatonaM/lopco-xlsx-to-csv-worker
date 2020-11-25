fb7e1fb54e8e60ea630d1562652d67a8d4a41747
y-du <42994541+y-du@users.noreply.github.com>
2020-11-25 15:19:34 +0100

    add line count

--- converter.sh
+++ converter.sh
@@ -31,6 +31,7 @@ echo "converting ..."
 if ssconvert --import-type="Gnumeric_Excel:xlsx" --export-type="Gnumeric_stf:stf_assistant" -O "separator=$delimiter quoting-on-whitespace=FALSE quoting-mode=never eol=unix format=raw charset=UTF-8" "$xlsx_file" "$out_file_name"; then
     head -5 "$out_file_name"
     line_count=$(( $(wc -l < "$out_file_name") - 1 ))
+    echo "total number of lines written:" $line_count
     if ! curl --header 'Content-Type: application/json' --data "{\""$DEP_INSTANCE"\": [{\"csv_file\": \""$out_file_name"\", \"line_count\": "$line_count"}]}" -X POST "$JOB_CALLBACK_URL"; then
         echo "callback failed"
         rm "$out_file_name"
